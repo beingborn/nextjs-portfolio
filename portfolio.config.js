9466b5af94427721437b49010e051112cee05a78
beingborn <dolmin0708@naver.com>
2025-09-15 18:11:48 +0900

    포트폴리오 전역 설정

---
+++ portfolio.config.js
@@ -0,0 +1,17 @@
+/** 
+ * Portfolio Configuration
+ * 
+ * Customize Portfolio by modifying this file.
+ */
+
+const portfolioConfig = {
+    // Social Links :: 소셜 링크
+    social : {
+        github: "https://github.com/beingborn",
+        email: "mindol0708@gmail.com",
+        blog :  "https://beingborn.tistory.com/"
+    }
+}
+
+export { portfolioConfig }
+
