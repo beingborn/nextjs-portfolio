563911e633d2e8738e17cf1637087c5b68ec435e
mhlee <160807673+beingborn@users.noreply.github.com>
2026-03-02 23:50:59 +0900

    feat: 컴포넌트 페이지 인덱스 페이지 생성

--- frontend/src/app/ui/page.tsx
+++ frontend/src/app/ui/page.tsx
@@ -1,20 +1,34 @@
+import { ExternalLink } from 'lucide-react';
 import Link from 'next/link';
 
 export default function UserInterface() {
     return (
-        <ul className="flex flex-col gap-2">
-            <li>
-                <Link href="/ui/button">Button</Link>
-            </li>
-            <li>
-                <Link href="/ui/loading">Loading</Link>
-            </li>
-            <li>
-                <Link href="/ui/custominput">Input</Link>
-            </li>
-            <li>
-                <Link href="/ui/tab">Tab</Link>
-            </li>
-        </ul>
+        <section>
+            <Link
+                className="text-white inline-flex items-center gap-2 border p-2 bg-secondary-500 hover:underline rounded-lg  transition-all duration-300"
+                href="https://699d535ab73546f406674150-uoqrcznzfx.chromatic.com/?path=/docs/ui-button--docs"
+            >
+                컴포넌트 스토리북 링크
+                <ExternalLink size={20} />
+            </Link>
+
+            <div className="p-4">
+                <p className="text-lg font-semibold mb-3">Storybook 도입 및 운영 요약</p>
+                <ul className="list-disc pl-5 space-y-2 text-text-main">
+                    <li>
+                        공용 UI 컴포넌트를 독립적으로 확인하고 문서화하기 위해 Storybook을
+                        도입했습니다.
+                    </li>
+                    <li>
+                        초기 설정을 직접 구성했고, 주요 컴포넌트(Button, Card, Input, TextArea)
+                        스토리를 작성했습니다.
+                    </li>
+                    <li>
+                        GitHub Actions 기반 CI로 Chromatic 자동 배포를 연결해 변경 사항을 리뷰
+                        가능한 형태로 운영하고 있습니다.
+                    </li>
+                </ul>
+            </div>
+        </section>
     );
 }
